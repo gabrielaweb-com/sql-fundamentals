10c068c22f73fd167d7e6c6a52c738d6f75fa125
Gabriela Favaretto <paesfavaretto@gmail.com>
2026-07-09 11:17:42 -0300

    Delete projects/project-01-basic-queries/exercises/exercise-02.sql

--- projects/project-01-basic-queries/exercises/exercise-02.sql
+++
@@ -1,9 +0,0 @@
--- Exercise 02
--- Display the name and unit price of peripherals with stock greater than five.
-
-SELECT
-    nome,
-    preco_unitario
-FROM produtos
-WHERE categoria = 'Periféricos'
-  AND estoque > 5;
