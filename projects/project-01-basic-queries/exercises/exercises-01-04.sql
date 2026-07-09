9a7633635b1b554388e22da7980b000aa083eaca
Gabriela Favaretto <paesfavaretto@gmail.com>
2026-07-09 12:53:19 -0300

    feat: add SQL exercises 01-04.sql

--- projects/project-01-basic-queries/exercises/exercises-01-04.sql
+++ projects/project-01-basic-queries/exercises/exercises-01-04.sql
@@ -3,7 +3,7 @@
 Project 01 - Basic SQL Queries
 Exercises 01–04
 
-Author: Gabriela Valois
+Author: Gabriela Favaretto
 Institution: Faculdade Infnet
 Database: SQLite
 ==================================================
